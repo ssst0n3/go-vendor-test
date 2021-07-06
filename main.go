a63489ce5bbcca04ff908ba29be9bbbb7b7ca7eb
Lei Wang <ssst0n3@gmail.com>
2021-07-06 17:23:04 +0800

    add test case

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "st0n3"
+import (
+	"st0n3"
+	"fmt"
+)
 
 func main() {
+    source()
     st0n3.Crash()
+}
+
+func source() {
+    sink()
+}
+
+func sink() {
+    fmt.Println("sink")
 }
